0798dcc33bf8c87b4ddf8719f15888aa464aba76
Ricky37501 <starsinthesummernight@gmail.com>
2020-12-15 17:50:06 +0800

    DONE：数据库查询

--- app/src/main/java/com/whutxhm/findroute/DBUtils.java
+++ app/src/main/java/com/whutxhm/findroute/DBUtils.java
@@ -2,6 +2,8 @@ package com.whutxhm.findroute;
 import android.content.Context;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 public class DBUtils {
@@ -16,7 +18,7 @@ public class DBUtils {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Connection connection = getConn("medical");
+                Connection connection = getConn("db_traffic");
                 if (connection==null){
                     ToastUtils.show(tContext,"数据库连接失败");
                 }
@@ -27,15 +29,14 @@ public class DBUtils {
         }).start();
     }
 
-    private static Connection getConn(String dbName){
-
+    public static Connection getConn(String dbName){
         Connection connection = null;
         try{
             Class.forName(driver);    //动态加载类
             String ip = "192.168.43.249";      //本机IP地址，每次更换网路都需做修改！！！手机和运行MySQL的电脑要处于同一网络
 
-            //尝试建立到给定数据库URL的连接
-            connection = DriverManager.getConnection("jdbc:mysql://"+ip+":3306/"+dbName+"?useSSL=true", user, password);
+            //尝试建立到给定数据库URL的连接(preparedStatement.setString设置的是中文字符必须在url后加seUnicode=true&characterEncoding=utf8，否则传出去的是乱码)
+            connection = DriverManager.getConnection("jdbc:mysql://"+ip+":3306/"+dbName+"?useSSL=true"+"&seUnicode=true&characterEncoding=utf8", user, password);
         }catch(Exception e){
             e.printStackTrace();
         }
--- app/src/main/java/com/whutxhm/findroute/ResultActivity.java
+++ app/src/main/java/com/whutxhm/findroute/ResultActivity.java
@@ -2,20 +2,100 @@ package com.whutxhm.findroute;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class ResultActivity extends AppCompatActivity {
+
+    private String mStartStation="友谊大道华城广场";
+    private String mEndStation="武汉火车站";
+    private ArrayList<String> mRouteOption=new ArrayList<>();
+    private Context rContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        rContext=this;
         setContentView(R.layout.activity_result);
         Intent intent=getIntent();
-        String startStation=intent.getStringExtra("start");
-        String endStation=intent.getStringExtra("end");
+        //mStartStation=intent.getStringExtra("start");
+        //mEndStation=intent.getStringExtra("end");
 
         //执行数据库查询炒作
-        DBUtils.testConnection(this);
+        //DBUtils.testConnection(this);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(findRoute()){
+                    ToastUtils.show(rContext,mRouteOption.get(0));
+
+                }
+            }
+        }).start();
+    }
+    public boolean findRoute(){
+        Connection connection = DBUtils.getConn("db_traffic");
+        try{
+            String sql = "SELECT DISTINCT r.routeID " +
+                    "FROM tb_route r " +
+                    "WHERE EXISTS( " +
+                    "  SELECT * " +
+                    "  FROM tb_sequence sec1 " +
+                    "  WHERE stationID in( " +
+                    "    SELECT stationID " +
+                    "    FROM tb_station " +
+                    "    WHERE stationName= ? " +
+                    "  )" +
+                    "  AND sec1.routeID=r.routeID " +
+                    ")" +
+                    "AND EXISTS( " +
+                    "  SELECT *" +
+                    "  FROM tb_sequence sec2 " +
+                    "  WHERE stationID in( " +
+                    "    SELECT stationID " +
+                    "    FROM tb_station " +
+                    "    WHERE stationName= ? " +
+                    "  ) " +
+                    "  AND sec2.routeID=r.routeID " +
+                    ") ";
+            if(connection != null){    //成功与数据库建立连接
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                if(preparedStatement != null){
+                    preparedStatement.setString(1, mStartStation);
+                    preparedStatement.setString(2, mEndStation);
+                    //执行sql查询语句并返回结果集
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if(resultSet != null){     //查询结果不为空
+                        while(resultSet.next()){
+                            String temp=resultSet.getString("routeID");
+                            mRouteOption.add(temp);
+                        }
+                        resultSet.close();
+                        preparedStatement.close();
+                        connection.close();
+                        return true;
+                    }else{
+                        ToastUtils.show(rContext,"查询结果为空");
+                        return false;//查询结果为空
+                    }
+                }else{
+                    ToastUtils.show(rContext,"执行语句准备出错");
+                    return false;//执行语句准备出错
+                }
+            }else{
+                ToastUtils.show(rContext,"数据库连接失败");
+                return false;//数据库连接失败
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            ToastUtils.show(rContext,"发生异常");
+            return false;//发生异常
+        }
     }
 }

368bd4e3f282da8ed8c28b9d680db8a337ffb0c8
Ricky37501 <starsinthesummernight@gmail.com>
2020-12-20 12:31:25 +0800

    加了个reverse出发站和终点站，方便验收的时候演示

--- app/src/main/java/com/whutxhm/findroute/MainActivity.java
+++ app/src/main/java/com/whutxhm/findroute/MainActivity.java
@@ -13,29 +13,36 @@ public class MainActivity extends AppCompatActivity {
     public  Context sContext;
     private EditText startStation;
     private EditText endStation;
-    private Button searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startStation=findViewById(R.id.star_point);
         endStation=findViewById(R.id.end_point);
-        searchButton=findViewById(R.id.search_btn);
+        Button searchButton = findViewById(R.id.search_btn);
+        Button reverseButton = findViewById(R.id.reverse_btn);
+        //预载数据库
         DBUtils.connect();
+        //设置按钮点击事件
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                    String start = startStation.getText().toString();
-                    String end = endStation.getText().toString();
-                    intent.putExtra("start", start);
-                    intent.putExtra("end", end);
-                    startActivity(intent);
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                String start = startStation.getText().toString();
+                String end = endStation.getText().toString();
+                intent.putExtra("start", start);
+                intent.putExtra("end", end);
+                startActivity(intent);
+            }
+        });
+        reverseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tempStart = startStation.getText().toString();
+                String tempEnd = endStation.getText().toString();
+                endStation.setText(tempStart);
+                startStation.setText(tempEnd);
             }
         });
     }
